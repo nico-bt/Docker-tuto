5c47ade7bd7593537ce6daf62930cca167d5ef80
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-03-06 22:12:14 -0300

    Basic api structure, to simulate a node aplication

---
+++ api/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json([
+    {
+      id: "1",
+      title: "Book Review: A Short History of Nearly Everything",
+    },
+    {
+      id: "2",
+      title: "Game Review: Alone in the dark",
+    },
+    {
+      id: "3",
+      title: "Show Review: Last of us",
+    },
+  ]);
+});
+
+app.listen(4000, () => {
+  console.log("listening for requests on port 4000");
+});
